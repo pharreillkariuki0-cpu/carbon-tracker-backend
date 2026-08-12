73a072944efff685596690cbdbad7562d2a6b54c
pharreillkariuki0-cpu <pharreillkariuki0@gmail.com>
2026-08-12 12:50:33 +0300

    Fix DJANGO_SETTING_MODULE path

--- codewave/manage.py
+++ codewave/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codewave.settings')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codewave.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codewave.codewave.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
