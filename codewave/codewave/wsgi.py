b504c2086ffc7c8936e1eb66b554afe38cc7cc87
pharreillkariuki0-cpu <pharreillkariuki0@gmail.com>
2026-08-19 13:38:50 +0300

    Made changes to manage and wsgi files

--- codewave/codewave/wsgi.py
+++ codewave/codewave/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codewave.codewave.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codewave.settings')
 
 application = get_wsgi_application()
